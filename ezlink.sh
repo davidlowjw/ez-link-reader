e997c65256f8b1ec2d4d22a61b28607207961ff0
David Low <davidlowjw@gmail.com>
2012-10-01 21:04:47 +0800

    Apended comments on PN532 & ISO/IEC 14443-B commands

--- ezlink.sh
+++ ezlink.sh
@@ -7,11 +7,19 @@ cat << EOF | \
     awk 'NR==5 {print substr($0,39,32)}'
 
 #Associate with the ISO-14443 type B card
-#Use InListPassiveTarget command
 4A 01 03 00
+#4A = Use InListPassiveTarget command
+#01 = Number of cards to initialize
+#03 = 106 kbps baud rate
+#00 = AFI
 
-#Use InDataExchange command - Retrieve id=3
+#Retrieve data from purseid 3
 40 01 90 32 03 00 00 00
+#40 = InDataExchange command
+#01 = Target no. 1
+#90 32 = ISO/IEC 14443-4 command
+#03 = Purse id 3
+#00 00 00 = p2, lc, unknown parameters. 
 
 EOF
 
